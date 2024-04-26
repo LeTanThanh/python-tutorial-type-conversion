f6128c334ea824f6218b7ef742f8bea86daf2f5f
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-26 22:11:17 +0700

    Introduction to type conversion in Python

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 if __name__ == "__main__":
-  print("Python Type Conversion")
+  # Introduction to type conversion in Python
+  # value = input("Enter a value: ")
+  # print(value)
+
+  price = int(input("Enter the price ($): "))
+  tax = int(input("Enter the tax rate (%): "))
+
+  tax_amount = price * tax / 100
+
+  print(f"The tax amount price is ${tax_amount}")
